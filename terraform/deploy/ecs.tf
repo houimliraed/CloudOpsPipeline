911c4921311593a0c4df1349fe43932ceb27a76a
houimliraed <houimliraed@outlook.fr>
2025-10-13 19:58:42 +0100

    added ecs task role

--- terraform/deploy/ecs.tf
+++ terraform/deploy/ecs.tf
@@ -2,23 +2,42 @@
 # ECS cluster to run the app on fargate
 #####
 
-resource "aws_iam_policy" "task_excution_role_policy" {
-    name = "${local.prefix}-task-exec-role-policy"
-    path = "/"
-    description = "allow ecs to retrieve images and add to logs"
-    policy = file("./templates/ecs/task-execution-role-policy.json")
-  
+resource "aws_iam_policy" "task_execution_role_policy" {
+  name        = "${local.prefix}-task-exec-role-policy"
+  path        = "/"
+  description = "allow ecs to retrieve images and add to logs"
+  policy      = file("./templates/ecs/task-execution-role-policy.json")
+
 }
 
-resource "aws_iam_role" "task-execution-role" {
+resource "aws_iam_role" "task_execution_role" {
 
-    name = "${local.prefix}-task-execution-role"
-    assume_role_policy = file("./templates/ecs/task-assume-role-policy.json")
+  name               = "${local.prefix}-task-execution-role"
+  assume_role_policy = file("./templates/ecs/task-assume-role-policy.json")
 }
 
 resource "aws_iam_role_policy_attachment" "task_execution_role" {
   role       = aws_iam_role.task_execution_role.name
   policy_arn = aws_iam_policy.task_execution_role_policy.arn
+}
+
+resource "aws_iam_role" "app_task" {
+
+  name               = "${local.prefix}-app-task"
+  assume_role_policy = file("./templates/ecs/task-assume-role-policy.json")
+
+}
+resource "aws_iam_policy" "task_ssm_policy" {
+  name        = "${local.prefix}-task-ssm-role-policy"
+  path        = "/"
+  description = "policy to allow system manager to execute in container"
+  policy      = file("./templates/ecs/task-ssm-policy.json")
+
+}
+
+resource "aws_iam_role_policy_attachment" "task-ssm-policy" {
+  role       = aws_iam_role.app_task.name
+  policy_arn = aws_iam_policy.task_ssm_policy.arn
 }
 
 
